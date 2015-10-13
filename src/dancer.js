9dee621719b475f56b264daf1f47c5fdc06b471f
ACP31 <aaronp48@gmail.com>
2015-10-13 17:55:19 -0500

    Added extra dancers with differnt colors and shapes

--- src/dancer.js
+++ src/dancer.js
@@ -29,4 +29,16 @@ Dancer.prototype.setPosition = function(top, left) {
   this.$node.css(styleSettings);
 };
 
+var SquareDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('squareDancer');
+};
+
+SquareDancer.prototype = Object.create(Dancer.prototype);
+SquareDancer.prototype.constructor = SquareDancer;
+
+
+
+
+
 
